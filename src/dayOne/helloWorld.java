6d53da5c79706f52ce8f586c42cafc2a6c665c53
AncaNemes <lia03ro@yahoo.com>
2021-04-24 15:42:46 -0500

    another commit

--- src/dayOne/helloWorld.java
+++ src/dayOne/helloWorld.java
@@ -3,5 +3,6 @@ package dayOne;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("adding to github now");
     }
 }
